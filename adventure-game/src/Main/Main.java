eb0c1a70fe6dacb0c8f200e9e04f9891d89850f6
Jacob Crider <jpc214@gmail.com>
2021-10-30 11:34:10 -0400

    initial push with main class

---
+++ adventure-game/src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        Random random = new Random();
+
+    }
+}
